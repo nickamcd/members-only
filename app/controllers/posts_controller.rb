40e00d3389f60815062ae54b5a1fe8590635fcd6
Nick McDaniel <nickamcd@gmail.com>
2021-02-23 12:28:57 -0800

    Added username to users table

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,11 +7,22 @@ class PostsController < ApplicationController
   end
 
   def create
-
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to root_url
+    else
+      render :new
+    end
   end
 
   def index
   
+  end
+
+  private
+
+  def post_params
+    params.requre(:post).permit(:user_id, :title, :body)
   end
 
 end
