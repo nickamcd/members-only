45408fbcd0e8e0f14d2e75d342178802a5488fe7
Nick McDaniel <nickamcd@gmail.com>
2021-02-23 16:39:49 -0800

    Added foreign key from users table to the posts tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_202649) do
+ActiveRecord::Schema.define(version: 2021_02_24_002530) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -34,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_02_23_202649) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
